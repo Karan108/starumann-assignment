64b29316bcf94bd55dd2b0640617adec64b28f8a
karan <karanraj.rj@nobroker.in>
2024-02-22 15:12:16 +0530

    filter changes

--- src/pages/PatientsList.jsx
+++ src/pages/PatientsList.jsx
@@ -49,7 +49,10 @@ function PatientsList() {
     <div
       className={`flex bg-lightGrey ${loading ? " h-screen" : ""} min-h-screen`}
     >
-      <div style={{ backgroundColor: "#003366" }} className="w-5pe"></div>
+      <div
+        style={{ backgroundColor: "#003366" }}
+        className="hidden w-5pe md:flex"
+      ></div>
       <div className="w-95pe">
         <div className="w-full p-5 mb-1 bg-white">
           <div className="flex items-center gap-2.5 text-grey">
@@ -63,11 +66,10 @@ function PatientsList() {
           </div>
         </div>
 
-        <div className="flex flex-col items-center justify-center p-11">
-          <div className="flex items-center w-full">
-            <div className="w-1/5">Filter by age</div>
+        <div className="flex flex-col items-center justify-center gap-5 p-3 md:p-11">
+          <div className="flex justify-center w-full gap-3">
+            <div>Filter by age</div>
             <div className="w-4/5">
-              {/* use rc slider */}
               <Slider
                 getAriaLabel={() => "Age Range"}
                 value={value}
